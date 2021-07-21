09df071142e700030eb971d607dbd778f1e1c6de
Tsotetsi <me@tsotetsithapeloo.co.za>
2021-07-21 10:13:17 +0200

    Initial commit.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import requests
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def get_chuck_norris_jokes():
+
+    api_url = "https://api.chucknorris.io/jokes/random"
+
+    response = requests.get(api_url).json()
+
+    return response
